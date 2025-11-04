daf30977ec9df0453880823035005640a441832c
Praveen Bhathiya <praveenbandara421@gmail.com>
2025-11-04 13:54:29 +0530

    created employee login page and css file

--- employee-portal/src/App.js
+++ employee-portal/src/App.js
@@ -1,36 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import EmployeeLogin from "./pages/EmployeeLogin";
-import EmployeeDashboard from "./pages/EmployeeDashboard";
-import GenreSelection from "./pages/GenreSelection";
-import ProtectedRoute from "./components/ProtectedRoute";
-import Navbar from "./components/Navbar";
-import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<EmployeeLogin />} />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <EmployeeDashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/genres"
-          element={
-            <ProtectedRoute>
-              <GenreSelection />
-            </ProtectedRoute>
-          }
-        />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
---
+++ employee-portal/src/pages/EmployeeLogin.js
@@ -0,0 +1,51 @@
+import React, { useState} from "react";
+import { useNavigate } from "react-router-dom";
+import "./EmployeeLogin.css";
+import { loginEmployee } from "../utils/api";
+
+function EmployeeLogin() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.prevenDefault();
+        const success = await loginEmployee(email, password);
+        if (success) {
+            navigate("/dashboard");
+        } else{
+            alert("Invalid Credentials");
+        }
+    }
+
+
+    return (
+        <div className="login-container">
+            <div className="login-box">
+                <h2>Employee Login</h2>
+                <form onSubmit={handleSubmit}>
+                    <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    />
+                    <input
+                    type="password"
+                    placeholder="PassWord"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    />
+                    <button type="submit">Login</button>
+
+
+                </form>
+            </div>
+        </div>
+
+    );
+}
+
+export default EmployeeLogin;
